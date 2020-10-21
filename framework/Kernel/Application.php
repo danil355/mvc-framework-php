49d1c9f9419a923c6e295ec3ab94ed8732a39f85
Danil <bashkov_2003@list.ru>
2020-10-21 23:56:36 +0600

    first commit

---
+++ framework/Kernel/Application.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Framework\Kernel;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+
+final class Application
+{
+    /*** @var self  */
+    protected static $app;
+    protected $request;
+    protected function __construct(ServerRequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
+    static function app(ServerRequestInterface $request = null)
+    {
+        if(self::$app)
+            return self::$app;
+
+        self::$app = new self($request);
+        return self::$app;
+    }
+
+    /**  TODO: Должен вернуть Response для emit */
+    function start()
+    {
+        echo "Да я молодец";
+    }
+
+
+    function request()
+    {
+        return $this->request;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
-echo __FILE__;
-echo $_SERVER['REQUEST_URI'];
+
+use \Laminas\Diactoros\Response;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use App\Framework\Kernel\Application;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+//echo $_SERVER['REQUEST_URI'];
+
+
+$request = ServerRequestFactory::fromGlobals();
+
+
+//$response = new Response();
+//
+//
+//$response
+//    ->getBody()
+//    ->write('Hello world');
+//
+//
+//(new SapiEmitter)->emit($response);
+
+Application::app($request)->start();
